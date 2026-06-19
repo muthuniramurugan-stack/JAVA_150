e50decfec91b020751c36b7a70a62af7555e7de3
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-06-19 22:07:48 +0530

    added

--- NumberBases/Ascendingarrange.java
+++ NumberBases/Ascendingarrange.java
@@ -1,5 +1,20 @@
 public class Ascendingarrange {
     public static void main(String[] args) {
-        int num;
+        int num = 12345;
+        boolean ascending = true;
+        while (num > 9) {
+            int last = num % 10;
+            int seconlast = (num / 10) % 10;
+            if (seconlast > last) {
+                ascending = false;
+                break;
+            }
+            num /= 10;
+        }
+        if (ascending) {
+            System.out.println("Ascending");
+        } else {
+            System.out.println("Descending");
+        }
     }
 }
