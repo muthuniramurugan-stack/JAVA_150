db98272dc70fd7be52d719d54fb88d1781866388
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-06-20 10:31:13 +0530

    added

---
+++ Loops/uniquecount.java
@@ -0,0 +1,20 @@
+// counts the number of unique digits
+public class uniquecount {
+    public static void main(String[] args) {
+        int num = 12345;
+        String n = num + "";
+        int ucount = 0;
+        for (int i = 0; i < n.length() - 1; i++) {
+            int count = 0;
+            for (int j = 0; j < n.length() - 1; j++) {
+                if (n.charAt(i) == n.charAt(j)) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                ucount++;
+            }
+        }
+        System.out.println(ucount);
+    }
+}
