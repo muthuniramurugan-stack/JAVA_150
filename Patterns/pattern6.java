22b3fc03681ad8da9d342b21f357c59be8bc6247
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-06-26 13:25:54 +0530

    added

---
+++ Patterns/pattern6.java
@@ -0,0 +1,27 @@
+/*
+
+12345
+ 2345
+  345
+   45
+    5
+
+
+*/
+
+package Patterns;
+
+public class pattern6 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <= n; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
