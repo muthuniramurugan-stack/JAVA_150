5e329361b774fb3a617e07aeb9d5949c4501e8ce
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-06-26 15:52:07 +0530

    added

---
+++ Patterns/pattern12.java
@@ -0,0 +1,46 @@
+/*
+
+* * * * * * * *  
+ * * * * * * *  
+  * * * * * *  
+   * * * * *  
+    * * * *  
+     * * *  
+      * *  
+       *  
+      * *  
+     * * *  
+    * * * *  
+   * * * * *  
+  * * * * * *  
+ * * * * * * * 
+ * * * * * * * 
+
+
+*/
+
+package Patterns;
+
+class pattern12 {
+    public static void main(String[] args) {
+        int n = 8;
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
