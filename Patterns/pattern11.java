b42a17ddc9fa85a04d1d217088ce7e23b756d600
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-06-26 15:33:07 +0530

    added

---
+++ Patterns/pattern11.java
@@ -0,0 +1,28 @@
+/*
+
+{ } 
+{ } {{ }} 
+
+
+*/
+
+package Patterns;
+
+public class pattern11 {
+    public static void main(String[] args) {
+        int n = 2;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                for (int k = 1; k <= j; k++) {
+                    System.out.print("{");
+                }
+                System.out.print(" ");
+                for (int k = 1; k <= j; k++) {
+                    System.out.print("}");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
